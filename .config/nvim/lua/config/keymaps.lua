6bb90511bc2dc20f9a5b830327332f2cf6dba893
Frank Wiles <frank@revsys.com>
2025-01-04 21:09:33 -0600

    Find TODOs easier

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -6,3 +6,14 @@ local map = vim.keymap.set
 map("x", "/", "gc", { remap = true })
 -- Normal mode for a single line
 map("n", "/", "gcc", { remap = true })
+
+-- Find TODOs and comment
+map("n", "<leader>ft",
+  function()
+    require('fzf-lua').grep({
+      search = 'TODO|HACK|PERF|NOTE|FIX',
+      no_esc = true
+    })
+  end,
+  { desc = "Find TODOs and annotations" }
+)
