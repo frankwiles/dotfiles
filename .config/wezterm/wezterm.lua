07c28b5c65f320257cc25ee65d49a17c3f19aec5
Frank Wiles <frank@revsys.com>
2025-01-05 10:03:16 -0600

    Add CMD+k to clear terminal

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,5 +1,5 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
+local act = wezterm.action
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -9,6 +9,15 @@ config.font = wezterm.font("Monaco")
 config.font_size = 18.0
 
 -- For example, changing the color scheme:
+
+config.keys = {
+	-- Clears the scrollback and viewport leaving the prompt line the new first line.
+	{
+		key = "k",
+		mods = "CMD",
+		action = act.ClearScrollback("ScrollbackAndViewport"),
+	},
+}
 
 -- and finally, return the configuration to wezterm
 return config
