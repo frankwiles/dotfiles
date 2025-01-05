2f917c21b91d30ec7fab583bc6a377af71b4fe82
Frank Wiles <frank@revsys.com>
2025-01-04 20:46:05 -0600

    Add better comment / uncomment

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+-- In lua/config/keymaps.lua
+local map = vim.keymap.set
+
+-- Better comment / uncomment
+-- For visual mode (commenting selected blocks)
+map("x", "/", "gc", { remap = true })
+-- Normal mode for a single line
+map("n", "/", "gcc", { remap = true })
