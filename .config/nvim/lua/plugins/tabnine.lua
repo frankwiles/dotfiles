5660f6ff4b8cd3f6dc53aa92625b55e8a3f956c7
Frank Wiles <frank@revsys.com>
2025-01-05 09:49:25 -0600

    Add tabnine and updates

--- .config/nvim/lua/plugins/tabnine.lua
+++ .config/nvim/lua/plugins/tabnine.lua
@@ -1,7 +1,7 @@
 return {
   "saghen/blink.cmp",
   optional = true,
-  dependencies = { "tzachar/cmp-tabnine", "saghen/blink.compat" },
+  dependencies = { "tzachar/cmp-tabnine", "saghen/blink.compat", "codota/tabnine-nvim" },
   opts = {
     sources = {
       compat = { "cmp_tabnine" },
